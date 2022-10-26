263ffd450454e00f7dcbe4644bfcbb3f06fdc414
lbroglio <lbroglio@iastate.edu>
2022-10-26 09:00:54 -0500

    Added Commenting and reorganized code

--- Matrix.cs
+++ Matrix.cs
@@ -3,31 +3,29 @@ using System.IO;
 using System.Collections.Generic;
 
 
-private class MatrixSize{
-    public readonly int numRows;
-    public readonly int numCols;
-
-    public MatrixSize(int numRows ,int numCols){
-        this.numRows = numRows;
-        this.numCols = numCols;
-       
-
-    }
-
-    public override String ToString(){
-        return string.Format("{0}x{1}",numRows,numCols);
-    }
-
-}
-
-
-
 public class Matrix<T>{
-    MatrixSize size;
-    //This needs  to be changed to accomondate different type of matrices - might need its own data type
-    T[,] contents;
-    T determinant;
+    //Stored the RowxColumn representation of the size of a matrix
+    private class MatrixSize{
+        public readonly int numRows;
+        public readonly int numCols;
 
+        public MatrixSize(int numRows ,int numCols){
+            this.numRows = numRows;
+            this.numCols = numCols;
+        
+
+        }
+
+        public override String ToString(){
+            return string.Format("{0}x{1}",numRows,numCols);
+        }
+
+    }
+    MatrixSize size;
+    T[,] contents;
+    bool isSquare;
+
+    //Determines the sizr of a matrix in a provided file
     private MatrixSize findSize(String filepath){
         StreamReader checkFrom = File.OpenText(filepath);
        
@@ -59,6 +57,7 @@ public class Matrix<T>{
     return new MatrixSize(rowNum,colNum);
     }
 
+    //Gets values for the matrix from a provided file
     private void getMatrixFromFile(String filepath){
         StreamReader getFrom = File.OpenText(filepath);
         for(int i=0; i<size.numRows; i++){
@@ -80,22 +79,29 @@ public class Matrix<T>{
         getFrom.Close();
     }
 
+    //Constructors
+
+    //Creates a Matrix with values from a file
     public Matrix(String filepath){
         size = findSize(filepath);
         contents = new T[size.numRows,size.numCols];
         getMatrixFromFile(filepath);
-
+        isSquare  = size.numRows == size.numCols;
     }
+    //Creates a matrix with values from  the provided array
     public Matrix(T[,] getMatrix){
-        size = new MatrixSize (getMatrix.length,getMatrix.GetLength(0));
-        contents = getMatrix.Clone();
+        size = new MatrixSize (getMatrix.Length,getMatrix.GetLength(0));
+        contents = (T[,]) getMatrix.Clone();
+        isSquare  = size.numRows == size.numCols;
     }
+    //Creates an empty Matrix
     public Matrix(int numRows,int numCols){
         size = new MatrixSize(numRows,numCols);
         contents = new T[size.numRows,size.numCols];
+        isSquare  = size.numRows == size.numCols;
     }
 
-
+    //Converts the data stored in the matrix to a string
     public override String ToString(){
         string toReturn = "";
         
@@ -112,6 +118,10 @@ public class Matrix<T>{
         } 
         }
         return toReturn;
+    }
+
+    public bool getIsSquare(){
+        return isSquare;
     }
 
 }
